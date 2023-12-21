66fc70a226a5fb7dab35fbc529ad055a26a44887
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-22 02:02:59 +0900

    :sparkles: 강의 Section 2까지 완료
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

---
+++ Django_Web_Programing/mysite/mysite/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse, HttpResponseNotFound
+
+
+def index(request):
+    return HttpResponse("main index")
+
+
+def error_404_view(request, exception):
+    return HttpResponseNotFound("The page is not found!")
