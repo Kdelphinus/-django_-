2e5314d107880bdebf88ba596a09883c6cb09b0e
Kdelphinus <kmj951015@gmail.com>
2024-03-12 15:25:26 +0900

    :sparkles: dealership 프로젝트 생성

---
+++ Django_Web_Programing/dealershop/inventory/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class MainView(TemplateView):
+    template_name = "inventory/main.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "myko"
+        return context
